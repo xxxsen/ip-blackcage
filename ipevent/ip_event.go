a5d1aef231e7a34701b6e020d7fe5ecba40328a8
xxxsen <naivesen@gmail.com>
2025-02-22 13:49:00 +0800

    refactor: ip detect

--- ipevent/ip_event.go
+++ ipevent/ip_event.go
@@ -37,44 +37,64 @@ func (r *ipEventReader) start(handler *pcap.Handle) {
 	}
 }
 
+func (r *ipEventReader) decodeNetInfo(packet gopacket.Packet) (string, string, uint16, uint16, bool) {
+	var srcip, dstip gopacket.Endpoint
+	var srcport, dstport uint16
+	extractFn := func() bool {
+		nl := packet.NetworkLayer()
+		if nl == nil {
+			return false
+		}
+		srcip, dstip = nl.NetworkFlow().Endpoints()
+		tpl := packet.TransportLayer()
+		if tpl == nil {
+			return false
+		}
+
+		if ly, ok := packet.Layer(layers.LayerTypeTCP).(*layers.TCP); ok {
+			if !ly.SYN {
+				return false
+			}
+			srcport = uint16(ly.SrcPort)
+			dstport = uint16(ly.DstPort)
+			return true
+		}
+		if ly, ok := packet.Layer(layers.LayerTypeUDP).(*layers.UDP); ok {
+			srcport = uint16(ly.SrcPort)
+			dstport = uint16(ly.DstPort)
+			return true
+		}
+		return false
+	}
+	if !extractFn() {
+		return "", "", 0, 0, false
+	}
+	return srcip.String(), dstip.String(), srcport, dstport, true
+}
+
 func (r *ipEventReader) handlePacket(packet gopacket.Packet) {
-	// 获取IP层和TCP层
-	ipLayer := packet.Layer(layers.LayerTypeIPv4)
-	if ipLayer == nil {
+	srcip, dstip, srcport, dstport, ok := r.decodeNetInfo(packet)
+	if !ok {
 		return
 	}
-	ip, _ := ipLayer.(*layers.IPv4)
-
-	//主动发起请求
-	if _, ok := r.c.exitIps[ip.SrcIP.String()]; ok {
+	if _, ok := r.c.exitIps[srcip]; ok {
 		return
 	}
-
-	tcpLayer := packet.Layer(layers.LayerTypeTCP)
-	if tcpLayer == nil {
-		return
-	}
-
-	tcp, _ := tcpLayer.(*layers.TCP)
-	if !tcp.SYN { //
-		return
-	}
-
-	if _, ok := r.c.portMap[uint16(tcp.DstPort)]; !ok {
+	if _, ok := r.c.portMap[uint16(dstport)]; !ok {
 		return
 	}
 	logutil.GetLogger(context.Background()).Debug("recv port scan request",
-		zap.String("src", fmt.Sprintf("%s:%d", ip.SrcIP.String(), tcp.SrcPort)),
-		zap.String("dst", fmt.Sprintf("%s:%d", ip.DstIP.String(), tcp.DstPort)),
+		zap.String("src", fmt.Sprintf("%s:%d", srcip, srcport)),
+		zap.String("dst", fmt.Sprintf("%s:%d", dstip, dstport)),
 	)
 	r.ipchain <- event.NewEventData(
 		string(event.EventTypePortScan),
 		time.Now().UnixMilli(),
 		&IPEventData{
-			SrcIP:   ip.SrcIP.String(),
-			DstIP:   ip.DstIP.String(),
-			SrcPort: uint16(tcp.SrcPort),
-			DstPort: uint16(tcp.DstPort),
+			SrcIP:   srcip,
+			DstIP:   dstip,
+			SrcPort: srcport,
+			DstPort: dstport,
 		},
 	)
 }
